a795d2bd21fee9f8956910075fc85ee1af6d93bc
Md Miyad Hossain <miyad.mh@gmail.com>
2021-02-07 22:45:38 +0600

    update all section

--- app.js
+++ app.js
@@ -1,44 +1,49 @@
+// to get search meals and  button function
 const searchBtn = document.getElementById('Searchbutton');
 searchBtn.addEventListener('click', () => {
-    const inputMeals = document.getElementById('inputMeals').value;
+    const inputMeals = document.getElementById('inputMeals');
     searchMeals(inputMeals);
+    
 })
 
-
-
-
 //search meals
-const searchMeals = mealName => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputMeals.value}`)
+const searchMeals = (mealsName) => {
+    
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealsName.value}`)
         .then(res => res.json())
         .then(data => displayMeals(data.meals))
         .catch(error => notFound(error))
+        document.getElementById('inputMeals').value = '';
+        
 }
 
-// not found
+// not found message
 const notFound = () => {
     document.getElementById('notFound').innerText = 'Items not found';
 }
 
-// display melas 
+// display meals
 const displayMeals = meals => {
     const mealsDiv = document.getElementById('mealsDiv');
     meals.forEach(meal => {
-        //console.log(meals);
         const singleMealDiv = document.createElement('div');
         const mealInfo = `
-            <div onclick="mealInfo('${meal.strMeal}')" class="card" style="width: 18rem;">
-            <img src="${meal.strMealThumb}" class="card-img-top">
-            <div class="card-body">
-            <h5>${meal.strMeal}</h5>
-            </div>
-            </div>
-            
+
+        <div onclick="mealInfo('${meal.strMeal}')" class="cols-md-3 rounded mealImgDiv">
+        <div class="col">
+        <img src="${meal.strMealThumb}" class="card-img-top ">
+        <div class="card-body bg-light mealDiv">
+          <h5 class="card-title">${meal.strMeal}</h5>
+        </div>
+        </div>
+        </div>  
         `
         singleMealDiv.innerHTML = mealInfo;
         mealsDiv.appendChild(singleMealDiv);
     });
+    
 }
+
 
 // meal info
 const mealInfo = name => {
@@ -51,59 +56,23 @@ const mealInfo = name => {
 // render ingredients info
 const renderIngredientsInfo = info => {
     info.forEach(ingreident => {
-        console.log(ingreident);
         const ingreidentsInfo = document.getElementById('ingreidentInfo');
-        const ing = [
-            ingreident.strIngredient1,
-            ingreident.strIngredient2,
-            ingreident.strIngredient3,
-            ingreident.strIngredient4,
-            ingreident.strIngredient5,
-            ingreident.strIngredient6,
-            ingreident.strIngredient7,
-            ingreident.strIngredient8,
-            ingreident.strIngredient9,
-            ingreident.strIngredient10,
-            ingreident.strIngredient11,
-            ingreident.strIngredient12,
-            ingreident.strIngredient13,
-            ingreident.strIngredient14,
-            ingreident.strIngredient15,
-            ingreident.strIngredient16,
-            ingreident.strIngredient17,
-            ingreident.strIngredient18,
-            ingreident.strIngredient19,
-            ingreident.strIngredient20
-        ]
-
-        ing.forEach(element => {
-            if (element === null) {
-                console.log('null')
-            }
-            else {
-                console.log(element)
-                ingreidentsInfo.innerHTML = `
-            <img src="${ingreident.strMealThumb}">
-            <h1>${ingreident.strMeal}</h1>
-            <h3>Ingreidents</h3>
-            <p>${element}</p>
+        ingreidentsInfo.innerHTML = `
+        <div class="infoDiv">
+        <img class="mealInfoImg" src="${ingreident.strMealThumb}">
+        <h3>${ingreident.strMeal}</h3>
+        <h5>Ingreidents</h5>
+        <p>${ingreident.strIngredient1}</p>
+        <p>${ingreident.strIngredient2}</p>
+        <p>${ingreident.strIngredient3}</p>
+        <p>${ingreident.strIngredient4}</p>
+        <p>${ingreident.strIngredient5}</p>
+        <p>${ingreident.strIngredient6}</p>
+        <p>${ingreident.strIngredient7}</p>
+        <p>${ingreident.strIngredient8}</p>
+        <p>${ingreident.strIngredient9}</p>
+        <p>${ingreident.strIngredient10}</p>
+        </div>
         `
-            }
-        });
-        // ingreidentsInfo.innerHTML = `
-        //     <img src="${ingreident.strMealThumb}">
-        //     <h1>${ingreident.strMeal}</h1>
-        //     <h3>Ingreidents</h3>
-        //     <p>${ingreident.strIngredient1}</p>
-        //     <p>${ingreident.strIngredient2}</p>
-        //     <p>${ingreident.strIngredient3}</p>
-        //     <p>${ingreident.strIngredient4}</p>
-        //     <p>${ingreident.strIngredient5}</p>
-        //     <p>${ingreident.strIngredient6}</p>
-        //     <p>${ingreident.strIngredient7}</p>
-        //     <p>${ingreident.strIngredient8}</p>
-        //     <p>${ingreident.strIngredient9}</p>
-        //     <p>${ingreident.strIngredient10}</p>
-        // `
     });
 }
